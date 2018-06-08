9c5d105d3ba9e1d7ca9b4b60b9b59bf337c6df54
Daniel Adeniji <dadeniji@UCOP.edu>
2018-06-08 11:29:21 -0700

    Adding git command files

---
+++ gitAdd.cmd
@@ -0,0 +1,3 @@
+git add dbo.sp_ScriptTablePrune.sql
+
+git add git*.cmd
---
+++ gitInit.cmd
@@ -0,0 +1,6 @@
+echo "# SQLServer.ScriptTablePruning" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/DanielAdeniji/SQLServer.ScriptTablePruning.git
+git push -u origin master
